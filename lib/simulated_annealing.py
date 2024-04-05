83351a5161c3794de7c523d5a0f0b2ed3b9e051e
Red-Fleet <anshul145263@gmail.com>
2024-04-05 12:47:15 +0530

    before sa-score, slow/fast and info commit

---
+++ lib/simulated_annealing.py
@@ -0,0 +1,9 @@
+class SimulatedAnnealing:
+    def __init__(self, fragments: list[str] = None):
+        self.fragments = fragments
+    
+    def 
+
+    def simulatedAnnealing(objective_fn, fragments: list, ligand_size:int, temp):
+        pass
+    
